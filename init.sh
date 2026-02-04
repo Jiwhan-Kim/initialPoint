7b785a01c4cb9fd822bbb1f0d4922704b5bf7acd
Jiwhan-Kim <jiwhan1015@yonsei.ac.kr>
2026-02-04 10:20:09 +0000

    feat - zsh config

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
-sudo apt update
-sudo apt install build-essential
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential -y
+sudo apt install neovim -y
 
+rm ~/.zshrc
+ln -s ~/initialPoint/.zshrc ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
